f7c111b7b7c9569662ee56ec3a24a96f5efd09d1
Foxieboy <jelle.vos@gmail.com>
2026-04-02 19:51:31 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = 'diesel-v1';
+const ASSETS = [
+  '/DieselMonitor/',
+  '/DieselMonitor/index.html',
+  '/DieselMonitor/manifest.json',
+  'https://fonts.googleapis.com/css2?family=DM+Mono:wght@300;400;500&family=Syne:wght@400;600;700;800&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/DieselMonitor/')))
+  );
+});
